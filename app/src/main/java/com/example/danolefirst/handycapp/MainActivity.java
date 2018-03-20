f4b5f0058af0eefe99928c110c90897fcada8d19
Andy Chen <achen6224@baysidehighschool.org>
2018-03-19 21:25:11 -0400

    SQLite Stuffs

--- app/src/main/java/com/example/danolefirst/handycapp/MainActivity.java
+++ app/src/main/java/com/example/danolefirst/handycapp/MainActivity.java
@@ -1,9 +1,23 @@
 package com.example.danolefirst.handycapp;
 
 import android.app.FragmentManager;
+import android.app.SearchManager;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteQuery;
+import android.database.sqlite.SQLiteQueryBuilder;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.widget.SearchView;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.MenuInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,15 +28,22 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        handleIntent(getIntent());
+
 
        /* FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -42,6 +63,18 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
+    @Override
+    protected void onNewIntent(Intent intent){
+        handleIntent(intent);
+    }
+
+    DatabaseTable db = new DatabaseTable(this);
+    private void handleIntent(Intent intent){
+        if(Intent.ACTION_SEARCH.equals(intent.getAction())){
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            Cursor c = db.getWordMatches(query, null);
+        }
+    }
 
     @Override
     public void onBackPressed() {
@@ -55,8 +88,15 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        SearchManager searchManager =
+                (SearchManager)getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView)menu.findItem(R.id.action_search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
         return true;
     }
 
@@ -95,5 +135,116 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+    public class DatabaseTable {
+        public Cursor getWordMatches(String query, String[] columns) {
+            String selection = COL_WORD + " Match ?";
+            String[] selectionArgs = new String[] {query+"*"};
+
+            return query(selection, selectionArgs, columns);
+        }
+        private Cursor query (String selection, String[] selectionArgs, String[] columns) {
+            SQLiteQueryBuilder builder = new SQLiteQueryBuilder();
+            builder.setTables(FTS_VIRTUAL_TABLE);
+
+            Cursor cursor = builder.query(mDatabaseOpenHelper.getReadableDatabase(),
+                    columns, selection, selectionArgs, null,null,null);
+
+            if (cursor ==null) {
+                return null;
+            } else if (!cursor.moveToFirst()) {
+                cursor.close();
+                return null;
+            }
+            return cursor;
+        }
+
+
+        private static final String TAG = "DictionaryDatabase";
+
+        //The columns we'll include in the dictionary table
+        public static final String COL_WORD = "WORD";
+        public static final String COL_DEFINITION = "DEFINITION";
+
+        private static final String DATABASE_NAME = "DICTIONARY";
+        private static final String FTS_VIRTUAL_TABLE = "FTS";
+        private static final int DATABASE_VERSION = 1;
+        private final DatabaseOpenHelper mDatabaseOpenHelper;
+
+        public DatabaseTable(Context context) {
+            mDatabaseOpenHelper = new DatabaseOpenHelper(context);
+        }
+        private class DatabaseOpenHelper extends SQLiteOpenHelper {
+
+            private final Context mHelperContext;
+            private SQLiteDatabase mDatabase;
+
+            private static final String FTS_TABLE_CREATE =
+                    "CREATE VIRTUAL TABLE " + FTS_VIRTUAL_TABLE +
+                            " USING fts3 (" +
+                            COL_WORD + ", " +
+                            COL_DEFINITION + ")";
+
+            DatabaseOpenHelper(Context context) {
+                super(context, DATABASE_NAME, null, DATABASE_VERSION);
+                mHelperContext = context;
+            }
+
+            @Override
+            public void onCreate(SQLiteDatabase db) {
+                mDatabase = db;
+                mDatabase.execSQL(FTS_TABLE_CREATE);
+            }
+
+            @Override
+            public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+                Log.w(TAG, "Upgrading database from version " + oldVersion + " to "
+                        + newVersion + ", which will destroy all old data");
+                db.execSQL("DROP TABLE IF EXISTS " + FTS_VIRTUAL_TABLE);
+                onCreate(db);
+            }
+            private void loadDictionary(){
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try{
+                            loadWords();
+                        } catch (IOException e){
+                            throw new RuntimeException(e);
+                        }
+
+                    }
+
+                }).start();
+            }
+            private void loadWords()throws IOException {
+                    final Resources resources = mHelperContext.getResources();
+                    InputStream inputStream = resources.openRawResource(R.raw.definitions);
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                    try  {
+                        String line;
+                    while ((line = reader.readLine()) !=null) {
+                        String[] strings = TextUtils.split(line,"-");
+                        if (strings.length <2)continue;
+                        long id = addWord(strings[0].trim(), strings[1].trim());
+                    }
+
+            } finally {
+                reader.close();
+            }
+        }
+        public long addWord(String word, String defintion) {
+            ContentValues initalValues = new ContentValues();
+            initalValues.put(COL_WORD, word);
+            initalValues.put(COL_DEFINITION, definiton);
+
+            return mDataBase.insert(FTS_VIRTUAL_TABLE,null, initalValues);
+
+        }
+
+
+
+
     }
 }
